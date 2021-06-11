3a83416f9f314bc40c4dfcf1ea32dc00e47ae4c4
LJPadilla <padillui001@gmail.com>
2021-06-10 21:00:29 -0700

    added gaming layer, changing lighting effect

--- keyboards/massdrop/alt/keymaps/LJPadilla/sunset.h
+++ keyboards/massdrop/alt/keymaps/LJPadilla/sunset.h
@@ -9,9 +9,9 @@ bool sunset(effect_params_t* params) {
     for (uint8_t i = led_min; i < led_max; i++) {
         RGB_MATRIX_TEST_LED_FLAGS();
         if (HAS_FLAGS(g_led_config.flags[i], LED_FLAG_MODIFIER)) {
-            rgb_matrix_set_color(i, 0, 0, 255);
-        } else {
             rgb_matrix_set_color(i, 255, 40, 0);
+        } else {
+            rgb_matrix_set_color(i, 0, 0, 255);
         }
     }
     return led_max < DRIVER_LED_TOTAL;
