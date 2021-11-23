c01f051c6ee86c91ac25962718000a7dd9d8006c
LJPadilla <padillui001@gmail.com>
2021-11-22 22:40:00 -0500

    still cannot fix persistant one-shot

--- keyboards/massdrop/alt/keymaps/LJPadilla/keymap.c
+++ keyboards/massdrop/alt/keymaps/LJPadilla/keymap.c
@@ -10,7 +10,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_65_ansi_blocker(
             KC_GRV,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  \
             KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_AMPR, \
-            OSL(2),     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_CIRC, \
+            TG(2),      KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_CIRC, \
             KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_ASTR, \
             KC_LCTL,    KC_LALT, KC_LGUI,                   KC_SPC,                          TG(1),  KC_RALT,              KC_LEFT, KC_DOWN, KC_RGHT \
             ),
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PERC, \
             _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, KC_EXLM, \
             _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_CAPS,          KC_PGUP, KC_AT, \
-            _______, _______, _______,                 KC_MPLY,                                     _______, _______,    KC_HOME, KC_PGDN, KC_END \
+            _______, _______, _______,                 KC_MPLY,                                       _______, _______, KC_HOME, KC_PGDN, KC_END \
             ),
 };
 
